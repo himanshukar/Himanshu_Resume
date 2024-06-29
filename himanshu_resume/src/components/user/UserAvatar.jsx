755b829f2d54501b98ef1917d578f88fdd518597
himanshu kar <51147786+himanshukar@users.noreply.github.com>
2024-06-29 20:15:01 +0530

    Update UserAvatar.jsx

--- himanshu_resume/src/components/user/UserAvatar.jsx
+++ himanshu_resume/src/components/user/UserAvatar.jsx
@@ -4,7 +4,7 @@ import {Avatar} from "@nextui-org/react";
 export default function UserAvatar() {
   return (
     <div className="flex gap-4 items-center absolute top-0 left-0">
-      <Avatar isBordered radius="sm" src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+      <Avatar isBordered radius="sm" src="src/assets/profilePic.png" />
     </div>
   );
 }
