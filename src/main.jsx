9173464ae08ded3fd92accabace07594448b9b26
himanshukar <himanshukar@rocketmail.com>
2024-06-30 11:32:52 +0530

    Added dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import {NextUIProvider} from '@nextui-org/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
+    <main className="dark text-foreground bg-background">
       <App />
+      </main>
       </NextUIProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
